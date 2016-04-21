3a4de8c2bf7e3dc7a205d0fa4f19fdb03700a009
Suniel Inigo <s_inigo@encs.concordia.ca>
2016-04-21 11:13:57 -0400

    first commit

---
+++ gipsy-src/src/gipsy/GEE/multitier/DST/ws/IWSDemandDispatcher.java
@@ -0,0 +1,19 @@
+package gipsy.GEE.multitier.DST.ws;
+
+import gipsy.GEE.IDP.DemandDispatcher.IDemandDispatcher;
+
+
+
+/**
+ * An interface for RMI Demand Dispatcher that extends the API from IDemandDispatcher interface 
+ *   and remote interface
+ * @author 
+ * @since 1.0.0
+ * @version $Id: IRMIDemandDispatcher.java,v :
+ */
+
+public interface IWSDemandDispatcher extends IDemandDispatcher{
+
+	
+	
+}
